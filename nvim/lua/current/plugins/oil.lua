2a6359b6e5f6044c978a0f07b58cf4e39a744fc8
Tim <tdeuken@gmail.com>
2025-02-01 21:41:35 -0700

    add and configured oil

---
+++ nvim/lua/current/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'stevearc/oil.nvim',
+     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      require('oil').setup{
+        float = {
+        -- Padding around the floating window
+        padding = 2,
+        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        max_width = 40,
+        max_height = 40,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+          }
+        }
+      }
+
+
+      vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+
+    end
+  }
+}
