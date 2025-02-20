ed12f2a81d14d0ae58d86f9e3ec01e38bda737f5
Tim <tdeuken@gmail.com>
2025-02-19 23:07:12 -1000

    add autocomplete

--- nvim/lua/current/plugins/lsp/lsp.lua
+++ nvim/lua/current/plugins/lsp/lsp.lua
@@ -14,9 +14,22 @@ return {
           },
         },
       },
+      {'saghen/blink.cmp'}
     },
-    config = function()
-      require("lspconfig").lua_ls.setup{}
+    opts = {
+          servers = {
+            lua_ls = {}
+          },
+    },
+    config = function(_, opts)
+      -- require("lspconfig").lua_ls.setup{}
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
     end
   }
 }
